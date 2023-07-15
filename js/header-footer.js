83f832fa334d9f48a19687f9d909f772003de1d8
EARTH <hiyudon0524@gmail.com>
2023-07-15 18:51:53 +0900

    s

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,22 +1,22 @@
 document.querySelector('header').innerHTML = `
 `;
 document.querySelector('footer').innerHTML = `
-  <div class="">
+  <div class="footer-style">
     <ul>
       <li>
         <a href="">お問い合わせ</a>
       </li>
-      <p class="">&#160;|&#160;</p>
+      <p class="stick">&#160;|&#160;</p>
       <li>-
         <a href="">プライバシーポリシー</a>
       </li>
-      <p class="">&#160;|&#160;</p>
+      <p class="stick">&#160;|&#160;</p>
       <li>
         <a href="">利用規約</a>
       </li>
     </ul>
         <div class="footer-border"></div>
-    <div class="footer-copylight">
+    <div class="footer-copy">
       <p>&copy;&#160;<div id="year"></div>&#160;<div>LIBERTIA</div></p>
     </div>
   </div>
